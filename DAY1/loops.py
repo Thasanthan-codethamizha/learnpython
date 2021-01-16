fbc64595855d0d6fd1ad3a8a740089e8d4ee4784
Code Thamizha_ Tech With Thasa <65809390+Thasanthan-codethamizha@users.noreply.github.com>
2021-01-16 12:23:50 +0530

    loops

---
+++ DAY1/loops.py
@@ -0,0 +1,23 @@
+for i in [0, 1, 2]:
+    print(i)
+
+print("*********************")
+
+for i in range(6):
+    print(i)
+
+print("*********************")
+
+list_name = ["Harry", "Ron", "Hermione", "Ginny"]
+i = 0
+for name in list_name:
+    i = i+1
+    print(f"number{i} :{name}")
+
+print("*********************")
+
+sname = "Harry"
+for letter in sname:
+    print(letter)
+
+print("*********************")
