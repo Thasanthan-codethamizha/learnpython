c3fb21f7fb379d9af5220f7ac9c0653518a0189b
Code Thamizha_ Tech With Thasa <65809390+Thasanthan-codethamizha@users.noreply.github.com>
2021-01-16 13:55:32 +0530

    decorators

---
+++ DAY1/decorators.py
@@ -0,0 +1,14 @@
+def announce(f):
+    def wrapper():
+        print("About to run the funtion......")
+        f()
+        print("Done with the function.")
+    return wrapper
+
+
+@announce
+def hello():
+    print("Hello,world!")
+
+
+hello()
